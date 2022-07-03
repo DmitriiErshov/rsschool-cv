aa842bd67f7390148c33d2bd0e9f2e95db19d70e
DmitriiErshov <dmitriiershovru@gmail.com>
2022-07-03 18:48:55 +0300

    fix: menu burger

--- Travel/js/script.js
+++ Travel/js/script.js
@@ -1,1 +1,15 @@
-$('.slider').slick();
+
+
+
+// menu burger
+
+const iconMenu = document.querySelector('.header__burger');
+if (iconMenu){
+    const menuBody = document.querySelector('.header__nav');
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('.lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+
+    });
+}
