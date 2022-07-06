ea98a4595f43bc2f1d72a201c1a7447807b11b71
DmitriiErshov <dmitriiershovru@gmail.com>
2022-07-06 16:04:13 +0300

    fix: link stories

--- Travel/js/script.js
+++ Travel/js/script.js
@@ -42,3 +42,17 @@ $(document).ready(function(){
         infinite: false,
     })
 })
+
+// ancher
+
+$(document).ready(function(){
+    $("a.nav-link").click(function(){
+        $("html, body").animate({
+            scrollTop: $($(this).attr("href")).offset().top + "px"
+        }, {
+            duration: 1000,
+            easing: "swing"
+        });
+        return false;
+    });
+})
